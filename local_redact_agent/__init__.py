0c95b7236f4b1ee5285feb5716f50b0c79e656bd
Arunavburagohainchop <arunav@chopchopfinance.com>
2026-04-18 01:38:31 +0530

    update name

--- local_redact_agent/__init__.py
+++ local_redact_agent/__init__.py
@@ -1,4 +1,4 @@
-"""Public package API for local-redact-agent."""
+"""Public package API for Redactora."""
 
 from local_redact_agent.fastapi import app, create_app
 from local_redact_agent.library import redact_pdf_document, redact_pdf_document_sync
